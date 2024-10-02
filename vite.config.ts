6d84cb9c8afbf7e4640af0d13d8f4685f99bbe0b
Xiao Zhong <zhongxiao2017@gmail.com>
2024-10-02 17:39:59 +1000

    update dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "vitest/config"; // for typescript
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -9,4 +8,17 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./config/vitest-setup.ts",
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
+  base: "/",
 });
